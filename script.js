ccd396004560113d82f07275b7747ce6e68ecef7
Gaurav Kumar Sharma <91082465+sharmag02@users.noreply.github.com>
2023-06-14 23:46:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -47,9 +47,11 @@ function showSlides() {
 
 
 
-
-
-
-
-
-  
+// Add an event listener to each skills-box element
+var skillsBoxes = document.getElementsByClassName("skills-box");
+for (var i = 0; i < skillsBoxes.length; i++) {
+  skillsBoxes[i].addEventListener("click", function() {
+    // Toggle the active class on the clicked skills-box
+    this.classList.toggle("active");
+  });
+}
