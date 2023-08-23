d4b81e0b44d05110218f92d961093910c71f2938
Surojit Mondal <surojitmondalit@gmail.com>
2023-08-24 01:32:04 +0530

    Initial commit by Surojit

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+window.addEventListener("load", () => {
+  document.querySelector("#preloader").style.cssText = "display:none;";
+});
+
+
+let loginpopup = document.querySelectorAll("#login");
+let registerpopup = document.querySelectorAll("#signup");
+
+let nextbutton_1 = document.querySelectorAll("#next-button-1");
+let nextbutton_2 = document.querySelectorAll("#next-button-2");
+
+let backbutton_1 = document.querySelectorAll("#back-button-1");
+let backbutton_2 = document.querySelectorAll("#back-button-2");
+
+
+function openLoginPopup() {
+  loginpopup.classList.add("open-popup");
+}
+function openSignupPopup() {
+  registerpopup.classList.add("open-popup");
+}
+function closePopup() {
+  loginpopup.classList.remove("open-popup");
+  registerpopup.classList.remove("open-popup");
+}
+function nextButton() {
+
+}
+function nextButton() {
+
+}
+
 
 // Add an event listener to each skills-box element
 var skillsBoxes = document.getElementsByClassName("skills-box");
@@ -9,3 +41,17 @@ for (var i = 0; i < skillsBoxes.length; i++) {
 }
 
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
